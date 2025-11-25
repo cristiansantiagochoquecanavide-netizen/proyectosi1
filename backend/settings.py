a61cf4d3515e96b9fbf26da1ddfbb57df8748c14
cristiansantiagochoquecanavide <cristiansantiagochoquecanavide@gmail.com>
2025-11-25 15:17:00 -0400

    Actualización de configuración y cambios recientes settings

--- backend/settings.py
+++ backend/settings.py
@@ -184,7 +184,7 @@ else:
         # Puertos alternativos cuando Vite se ejecuta en otro puerto
         'http://localhost:5174',
         'http://127.0.0.1:5174',
-        'https://proyectosi1-cs32.onrender.com',
+        'https://proyectosi1-6bow.onrender.com',
         'https://consultoriodental.onrender.com'
     ]
 
